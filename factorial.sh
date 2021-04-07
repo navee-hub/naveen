ca89b634bd42ba4df0e1cd7627ea6ca14c0f15de
Naveen <naveenskpn1@gmail.com>
2021-04-07 03:35:54 +0000

    master changes

--- factorial.sh
+++ factorial.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 num=$1
 fact=1
-while [ $num -gt 1 ]
+while [ $num -gt 0 ]
 do
 fact=` expr $num \* $fact `
-num=` expr $num - 1 `
+num=` expr $num - 0 `
 done
 echo " factorial of given number is: $fact "
